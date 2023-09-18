9deba9c94b13f547cdadfa7f52faf3d4726121bb
sourav-dhar <dharsourav03@gmail.com>
2023-09-18 08:36:25 +0530

    Data Ingestion

---
+++ src/mlproject/utils.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dotenv import load_dotenv
+import pymysql
+
+load_dotenv()
+
+host = os.getenv("host")
+user = os.getenv("user")
+password = os.getenv("password")
+db = os.getenv("db")
+
+def read_sql_data():
+    logging.info("reading sql database started")
+    try:
+        mydb = pymysql.connect(
+            host = host,
+            user = user,
+            password = password,
+            db = db
+        )
+        logging.info("connection established", mydb)
+        df = pd.read_sql_query("SELECT * FROM STUDENTS", mydb)
+        print(df.head())
+        
+        return df
+        
+        
+    except Exception as ex:
+        raise CustomException(ex)
+    
